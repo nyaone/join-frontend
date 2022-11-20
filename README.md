1c6fae185df7efe2df2159ca1aeb0b9b716ec9d4
Nya Candy <dev@candinya.com>
2022-11-21 00:29:28 +0800

    feat: enable specify backend uri

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Join NyaOne
+
+## 设置
+
+构建时指定 `REACT_APP_BACKEND_URI` 环境变量为您的目标 [join-backend](https://github.com/nyaone/join-backend) 服务器地址。
--- src/common/settings.ts
+++ src/common/settings.ts
@@ -1,3 +1,3 @@
-export const BackendUri = 'http://localhost:8080';
+export const BackendUri = process.env.REACT_APP_BACKEND_URI || 'http://localhost:8080';
 export const AdminSessionKey = 'admin-session';
 export const AdminUsernameKey = 'admin-username';
