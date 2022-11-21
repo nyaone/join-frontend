1b4e9336bc7441098b48f41657be917494991aa7
Nya Candy <dev@candinya.com>
2022-11-21 14:25:17 +0800

    fix(ui): remove unwanted header bottom line

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -14,7 +14,7 @@ const Header = ({ loginButton }: HeaderProps) => {
     <>
       <Popover className="relative bg-white">
         <div className="mx-auto max-w-7xl px-4 sm:px-6">
-          <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <Link to="/">
                 <span className="sr-only">NyaOne</span>
