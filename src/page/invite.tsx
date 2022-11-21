427efb47eaec2ef6e363e8ca47210534a08626a1
Nya Candy <dev@candinya.com>
2022-11-21 22:34:56 +0800

    fix: don't disable scroll on extra small screens
    but who would use such a small screen like that?

--- src/page/invite.tsx
+++ src/page/invite.tsx
@@ -134,7 +134,7 @@ const Invite = () => {
   return (
     <>
       <main>
-        <div className="flex min-h-[100vh] overflow-hidden">
+        <div className="flex min-h-[100vh]">
           <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
             <div className="mx-auto w-full max-w-sm lg:w-96">
               <div>
