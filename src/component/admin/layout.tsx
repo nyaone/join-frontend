3567d4cbf014af70c1c7a614828f57c928f05628
Nya Candy <dev@candinya.com>
2022-12-20 18:55:01 +0800

    chore: wrap logout confirm modal

--- src/component/admin/layout.tsx
+++ src/component/admin/layout.tsx
@@ -15,6 +15,7 @@ import NyaOneLogo from '@/asset/NyaOneLogo';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import API from '@/common/api';
 import { AdminSessionKey, AdminUsernameKey } from '@/common/settings';
+import ModalWrapper from '@/component/modal/modalWrapper';
 
 const navigation = [
   { name: '邀请码', href: '/admin/codes', icon: CodeBracketIcon },
@@ -174,65 +175,35 @@ interface LogoutCheckProps {
 const LogoutCheck = ({ open, setOpen, onConfirm }: LogoutCheckProps) => {
   const cancelButtonRef = useRef(null);
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-                <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
-                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                      是否确认登出
-                    </Dialog.Title>
-                  </div>
-                </div>
-                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
-                    onClick={() => onConfirm()}
-                  >
-                    确认登出
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
-                  >
-                    取消
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+    <ModalWrapper isOpen={open} onClose={setOpen} className={'sm:w-full sm:max-w-sm'}>
+      <div>
+        <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
+          <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
         </div>
-      </Dialog>
-    </Transition.Root>
+        <div className="mt-3 text-center sm:mt-5">
+          <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+            是否确认登出
+          </Dialog.Title>
+        </div>
+      </div>
+      <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+        <button
+          type="button"
+          className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
+          onClick={() => onConfirm()}
+        >
+          确认登出
+        </button>
+        <button
+          type="button"
+          className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
+          onClick={() => setOpen(false)}
+          ref={cancelButtonRef}
+        >
+          取消
+        </button>
+      </div>
+    </ModalWrapper>
   );
 };
 
