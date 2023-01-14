13147b5b4df877f11250ae44b570391010839e3a
Nya Candy <dev@candinya.com>
2023-01-14 16:39:15 +0800

    chore: split menu

--- src/component/admin/layout.tsx
+++ src/component/admin/layout.tsx
@@ -26,11 +26,127 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-interface SideBarProps {
+interface MobileMenuProps {
   sidebarOpen: boolean;
-  setSidebarOpen: Dispatch<SetStateAction<boolean>>;
+  setSidebarOpen: (state: boolean) => void;
+  locationPath: string;
+}
+const MobileMenu = ({ sidebarOpen, setSidebarOpen, locationPath }: MobileMenuProps) => (
+  <Transition.Root show={sidebarOpen} as={Fragment}>
+    <Dialog as="div" className="relative z-40 lg:hidden" onClose={setSidebarOpen}>
+      <Transition.Child
+        as={Fragment}
+        enter="transition-opacity ease-linear duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity ease-linear duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+      </Transition.Child>
+
+      <div className="fixed inset-0 z-40 flex">
+        <Transition.Child
+          as={Fragment}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
+          <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-sky-700 pt-5 pb-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="absolute top-0 right-0 -mr-12 pt-2">
+                <button
+                  type="button"
+                  className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  onClick={() => setSidebarOpen(false)}
+                >
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </button>
+              </div>
+            </Transition.Child>
+            <div className="flex flex-shrink-0 items-center px-4">
+              <NyaOneLogo className={'h-8 w-auto text-white'} />
+            </div>
+            <nav className="mt-5 h-full flex-shrink-0 divide-y divide-sky-800 overflow-y-auto" aria-label="Sidebar">
+              <div className="space-y-1 px-2">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    className={classNames(
+                      locationPath === item.href
+                        ? 'bg-sky-800 text-white'
+                        : 'text-sky-100 hover:bg-sky-600 hover:text-white',
+                      'group flex items-center rounded-md px-2 py-2 text-base font-medium',
+                    )}
+                    aria-current={locationPath === item.href ? 'page' : undefined}
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <item.icon className="mr-4 h-6 w-6 flex-shrink-0 text-sky-200" aria-hidden="true" />
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </nav>
+          </Dialog.Panel>
+        </Transition.Child>
+        <div className="w-14 flex-shrink-0" aria-hidden="true">
+          {/* Dummy element to force sidebar to shrink to fit close icon */}
+        </div>
+      </div>
+    </Dialog>
+  </Transition.Root>
+);
+
+interface DesktopMenuProps {
+  locationPath: string;
 }
 
+const DesktopMenu = ({ locationPath }: DesktopMenuProps) => (
+  <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col">
+    <div className="flex flex-grow flex-col overflow-y-auto bg-sky-700 pt-5 pb-4">
+      <div className="flex flex-shrink-0 items-center px-4">
+        <NyaOneLogo className={'h-8 w-auto text-white'} />
+      </div>
+      <nav className="mt-5 flex flex-1 flex-col divide-y divide-sky-800 overflow-y-auto" aria-label="Sidebar">
+        <div className="space-y-1 px-2">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              to={item.href}
+              className={classNames(
+                locationPath === item.href ? 'bg-sky-800 text-white' : 'text-sky-100 hover:bg-sky-600 hover:text-white',
+                'group flex items-center rounded-md px-2 py-2 text-sm font-medium leading-6',
+              )}
+              aria-current={locationPath === item.href ? 'page' : undefined}
+            >
+              <item.icon className="mr-4 h-6 w-6 flex-shrink-0 text-sky-200" aria-hidden="true" />
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </div>
+  </div>
+);
+
+interface SideBarProps {
+  sidebarOpen: boolean;
+  setSidebarOpen: (state: boolean) => void;
+}
 const SideBar = ({ sidebarOpen, setSidebarOpen }: SideBarProps) => {
   const location = useLocation();
   const nav = useNavigate();
@@ -55,113 +171,8 @@ const SideBar = ({ sidebarOpen, setSidebarOpen }: SideBarProps) => {
 
   return (
     <>
-      <Transition.Root show={sidebarOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-40 lg:hidden" onClose={setSidebarOpen}>
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 z-40 flex">
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-sky-700 pt-5 pb-4">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                    </button>
-                  </div>
-                </Transition.Child>
-                <div className="flex flex-shrink-0 items-center px-4">
-                  <NyaOneLogo className={'h-8 w-auto text-white'} />
-                </div>
-                <nav className="mt-5 h-full flex-shrink-0 divide-y divide-sky-800 overflow-y-auto" aria-label="Sidebar">
-                  <div className="space-y-1 px-2">
-                    {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        to={item.href}
-                        className={classNames(
-                          location.pathname === item.href
-                            ? 'bg-sky-800 text-white'
-                            : 'text-sky-100 hover:bg-sky-600 hover:text-white',
-                          'group flex items-center rounded-md px-2 py-2 text-base font-medium',
-                        )}
-                        aria-current={location.pathname === item.href ? 'page' : undefined}
-                        onClick={() => setSidebarOpen(false)}
-                      >
-                        <item.icon className="mr-4 h-6 w-6 flex-shrink-0 text-sky-200" aria-hidden="true" />
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
-                </nav>
-              </Dialog.Panel>
-            </Transition.Child>
-            <div className="w-14 flex-shrink-0" aria-hidden="true">
-              {/* Dummy element to force sidebar to shrink to fit close icon */}
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
-
-      {/* Static sidebar for desktop */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex flex-grow flex-col overflow-y-auto bg-sky-700 pt-5 pb-4">
-          <div className="flex flex-shrink-0 items-center px-4">
-            <NyaOneLogo className={'h-8 w-auto text-white'} />
-          </div>
-          <nav className="mt-5 flex flex-1 flex-col divide-y divide-sky-800 overflow-y-auto" aria-label="Sidebar">
-            <div className="space-y-1 px-2">
-              {navigation.map((item) => (
-                <Link
-                  key={item.name}
-                  to={item.href}
-                  className={classNames(
-                    location.pathname === item.href
-                      ? 'bg-sky-800 text-white'
-                      : 'text-sky-100 hover:bg-sky-600 hover:text-white',
-                    'group flex items-center rounded-md px-2 py-2 text-sm font-medium leading-6',
-                  )}
-                  aria-current={location.pathname === item.href ? 'page' : undefined}
-                >
-                  <item.icon className="mr-4 h-6 w-6 flex-shrink-0 text-sky-200" aria-hidden="true" />
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </nav>
-        </div>
-      </div>
+      <MobileMenu sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} locationPath={location.pathname} />
+      <DesktopMenu locationPath={location.pathname} />
     </>
   );
 };
