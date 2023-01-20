a710f230623954dd00c144bc6b85ef4d05dd8075
Nya Candy <dev@candinya.com>
2023-01-20 12:16:50 +0800

    chore: remove web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Layout from '@/layout';
 
 const container = document.getElementById('root');
@@ -11,8 +10,3 @@ root.render(
     <Layout />
   </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
